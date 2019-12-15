b2929deb0059ff8cf7356932fc3c8d4786e1da1c
ivan-alfonso <ivandaalf@hotmail.com>
2019-12-14 19:26:04 -0500

    PostgreSQL settings.py

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "<nombrebd>",
+        "USER": "<usuario>",
+        "PASSWORD": "<clave>",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
